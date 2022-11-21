e785c06995e043bb78f80190518617077a8d9b0e
ssrebrev <ssrebrev@vmware.com>
2022-11-21 04:00:46 +0200

    Fix comment

--- queue.go
+++ queue.go
@@ -144,8 +144,8 @@ func (qw *Writer[V]) enqueue(value V) (*segment[V], uint64) {
 
 func (qr *Reader[V]) dequeue(block bool) (uint64, V, bool) {
 	if qr.readIndex == segmentSize {
-		// qr.segment.next is never nil because previous segment slot seg.slots[segmentSize-1].flag
-		// was set which happens AFTER seg.next is set. See Enqueue method
+		// qr.segment.next is not nil at this point because previous segment slot seg.slots[segmentSize-1].flag
+		// was set which happens AFTER seg.next is set. See enqueue method
 		next := qr.segment.next.Load()
 		qr.segment = next
 		qr.readIndex = 0
