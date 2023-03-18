e0d16f20847ce485f7583b56f201b3ef5166e456
ssrebrev <ssrebrev@vmware.com>
2023-03-18 12:01:41 +0200

    Add generic TryDequeue func with cancellation channel

--- queue.go
+++ queue.go
@@ -1,32 +1,5 @@
-// BSD 3-Clause License
-//
-// Copyright (c) 2022, Svetoslav Srebrev <srebrev dot svetoslav at gmail dot com>
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-//
-// 1. Redistributions of source code must retain the above copyright notice, this
-// list of conditions and the following disclaimer.
-//
-// 2. Redistributions in binary form must reproduce the above copyright notice,
-// this list of conditions and the following disclaimer in the documentation
-// and/or other materials provided with the distribution.
-//
-// 3. Neither the name of the copyright holder nor the names of its
-// contributors may be used to endorse or promote products derived from
-// this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// Copyright (c) 2022, Svetoslav Srebrev <srebrev dot svetoslav at gmail dot com>. All rights reserved.
+// Use of this source code is governed by a 3-Clause license that can be found in the LICENSE file.
 
 package pq
 
@@ -44,6 +17,8 @@ const (
 	fWaiting = uint32(1)
 	fReady   = uint32(2)
 )
+
+var ErrCanceled = errors.New("operation canceled")
 
 type slot[V any] struct {
 	data V
@@ -111,12 +86,58 @@ func (qr *Reader[V]) TryDequeueWithIndex() (uint64, V, bool) {
 }
 
 func (qr *Reader[V]) DequeueCtx(ctx context.Context) (V, error) {
-	_, val, err := qr.dequeueCtx(ctx)
+	_, val, err := qr.DequeueCtxWithIndex(ctx)
 	return val, err
 }
 
 func (qr *Reader[V]) DequeueCtxWithIndex(ctx context.Context) (uint64, V, error) {
-	return qr.dequeueCtx(ctx)
+	index, val, err := TryDequeue(qr, ctx.Done())
+	if err != nil {
+		contextErr := ctx.Err()
+		if contextErr != nil {
+			err = contextErr
+		}
+	}
+
+	return index, val, err
+}
+
+func TryDequeue[V, C any](qr *Reader[V], chCancel <-chan C) (uint64, V, error) {
+	if qr.readIndex == segmentSize {
+		// qr.segment.next is not nil at this point because previous segment slot seg.slots[segmentSize-1].flag
+		// was set which happens AFTER seg.next is set. See enqueue method
+		next := qr.segment.next.Load()
+		if next == nil {
+			panic("nil next segment")
+		}
+		qr.segment = next
+		qr.readIndex = 0
+	}
+
+	// index < segmentSize
+	if qr.segment.slots[qr.readIndex].flag.Load() != fReady {
+
+		prev := qr.segment.slots[qr.readIndex].flag.Swap(fWaiting)
+		if prev != fReady {
+			select {
+			case <-chCancel:
+				swapped := qr.segment.slots[qr.readIndex].flag.CompareAndSwap(fWaiting, fClear)
+				if !swapped {
+					// prev == fReady. The item was set concurrently with chCancel.
+					// Consume qr.chReady to prevent next item from being read before it is first set.
+					<-qr.chReady
+				}
+				var zeroValue V
+				return 0, zeroValue, ErrCanceled
+
+			case <-qr.chReady:
+			}
+		}
+	}
+
+	index := qr.readIndex
+	qr.readIndex += 1
+	return qr.segment.offset + index, qr.segment.slots[index].data, nil
 }
 
 func (qw *Writer[V]) enqueue(value V) (*segment[V], uint64) {
@@ -171,46 +192,6 @@ func (qr *Reader[V]) dequeue(block bool) (uint64, V, bool) {
 	index := qr.readIndex
 	qr.readIndex += 1
 	return qr.segment.offset + index, qr.segment.slots[index].data, true
-}
-
-func (qr *Reader[V]) dequeueCtx(ctx context.Context) (uint64, V, error) {
-	if qr.readIndex == segmentSize {
-		// qr.segment.next is not nil at this point because previous segment slot seg.slots[segmentSize-1].flag
-		// was set which happens AFTER seg.next is set. See enqueue method
-		next := qr.segment.next.Load()
-		qr.segment = next
-		qr.readIndex = 0
-	}
-
-	// index < segmentSize
-	if qr.segment.slots[qr.readIndex].flag.Load() != fReady {
-
-		prev := qr.segment.slots[qr.readIndex].flag.Swap(fWaiting)
-		if prev != fReady {
-			select {
-			case <-ctx.Done():
-				prev = qr.segment.slots[qr.readIndex].flag.Swap(fClear)
-				if prev != fReady {
-					err := ctx.Err()
-					if err == nil {
-						// ctx.Done() is closed/signalled but, but ctx.Err() is nil? context.Context interface custom implementation bug?
-						err = errors.New("operation canceled")
-					}
-
-					var zeroValue V
-					return 0, zeroValue, err
-				}
-				// prev == fReady. The item was set concurrently with ctx.Done() channel.
-				// Consume qr.chReady and pretend we noticed qr.chReady first
-				<-qr.chReady
-			case <-qr.chReady:
-			}
-		}
-	}
-
-	index := qr.readIndex
-	qr.readIndex += 1
-	return qr.segment.offset + index, qr.segment.slots[index].data, nil
 }
 
 func (qw *Writer[V]) setNextSegment(seg *segment[V]) *segment[V] {
